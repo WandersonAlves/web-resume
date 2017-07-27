74582909df9a2d06e0ca1adb0983eff2fb2cd12b
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-07-26 23:49:05 -0300

    refactor: remove fa es6 import

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import fa from 'font-awesome/css/font-awesome.css';
 
 Vue.use(bootstrap);
-Vue.use(fa);
 
 new Vue({
   el: '#app',
