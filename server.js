de1feda8cdd6719948a364eb361041b20041fcf7
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-07-26 23:45:28 -0300

    chore(build): added node server

---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require("express"),
+    app = express(),
+    compression = require("compression"),
+    path = require("path");
+
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.NODE_ENV;
+
+app.use(compression());
+app.use('/', express.static(__dirname));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + `/index.html`));
+});
+
+app.listen(PORT);
+
+console.log(`Runing at port ${PORT} and in ${ENV} enviroment`);
