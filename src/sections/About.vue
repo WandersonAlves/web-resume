7aedc10a3e3ac0336e848fea53240130de5a5f02
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-08-14 23:21:33 -0300

    feat(routes): add skills page

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -1,7 +1,4 @@
 <style scoped lang="sass">
-.color 
-  background-color: yellow
-
 .container
   width: 100vw
   height: 100vh
@@ -9,20 +6,30 @@
 </style>
 <template>
   <section>
-    <div class="container color container-padding-top">
+    <div class="container container-padding-top">
       <h1>Under Construction</h1>
     </div>
+    <arrow class="black" @click.native="nextPage()"></arrow>
   </section>
 </template>
 <script>
+  import Arrow from '../components/Arrow.vue';
   export default {
     name: 'About',
-    created() {
+    components: {
+      Arrow
+    },
+    mounted() {
       //this.$bus.$emit('change-animation');
       this.$bus.$emit('route-text', 'ABOUT');
     },
     beforeDestroy() {
       this.$bus.$emit('route-text', '');
+    },
+    methods: {
+       nextPage() {
+        this.$router.push({ name: 'skills' });
+      }
     }
   }
 </script>
---
+++ src/sections/Skills.vue
@@ -0,0 +1,25 @@
+<style scoped lang="sass">
+.container
+  width: 100vw
+  height: 100vh
+
+</style>
+<template>
+  <section>
+    <div class="container bg-yellow container-padding-top">
+      <h1>Under Construction</h1>
+    </div>
+  </section>
+</template>
+<script>
+  export default {
+    name: 'Skills',
+    mounted() {
+      //this.$bus.$emit('change-animation');
+      this.$bus.$emit('route-text', 'SKILLS');
+    },
+    beforeDestroy() {
+      this.$bus.$emit('route-text', '');
+    }
+  }
+</script>
