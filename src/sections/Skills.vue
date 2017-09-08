23fd394a66b198722755cd700ee2349c09e6aa57
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-09-07 23:33:11 -0300

    feat(i18n): add i18n as computed

--- src/sections/Skills.vue
+++ src/sections/Skills.vue
@@ -10,17 +10,17 @@
             <h1 class="montserrat heading-margin light-pink heading-bg">- {{'skillsHeader' | translate | uppercase}} -</h1>
           </div>
           <div class="margin-top-20 flex flex-dir-row flex-flow-wrap-row">
-            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="95" skill="JavaScript" description="Closures, functional programming, Object.assign({}, obj)"></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="95" skill="JavaScript" :description="skillsJavascript"></circle-progress>
             <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="95" skill="VueJS" description="Vuex, vue-router, mixins..."></circle-progress>
             <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="89" skill="Git" description="Rebase, cherry-pick, merges..."></circle-progress>
             <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="80" skill="Jenkins" description="Pipelines, Groovy Files"></circle-progress>
-            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="75" skill="HTML 5" description="Sites pessoais, corporativos, etc.."></circle-progress>
-            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="75" skill="Photoshop" description="Prototipos, tratamento e manipulação de imagens"></circle-progress>
-            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="63" skill="CSS3" description="Clip-path, borders, customizações"></circle-progress>
-            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="55" skill="NodeJS" description='"É tudo JavaScript mesmo..."'></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="75" skill="HTML 5" :description="skillsHtml"></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="75" skill="Photoshop" :description="skillsPhotoshop"></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="63" skill="CSS3" description="Clip-path, borders, media-queries"></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="55" skill="NodeJS" :description="skillsNode"></circle-progress>
             <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="50" skill="Angular 4" description="TypeScript"></circle-progress>
-            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="10" skill="React" description="Learning..."></circle-progress>
-            <circle-progress class="flex-33" direction="mid" color="#ff9c00" :percentage="100" skill="Beber" description="Cerveja, Vodka, Vinho..."></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#514444" :percentage="10" skill="React" :description="skillsReact"></circle-progress>
+            <circle-progress class="flex-33" direction="mid" color="#ff9c00" :percentage="100" :skill="skillBeerTitle" :description="skillsBeer"></circle-progress>
           </div>
         </div>
       </div>
@@ -34,6 +34,29 @@
     name: 'Skills',
     components: {
       CircleProgress
+    },
+    computed: {
+      skillsJavascript() {
+        return this.$t('skillJavascript');
+      },
+      skillsHtml() {
+        return this.$t('skillsHtml');
+      },
+      skillsPhotoshop() {
+        return this.$t('skillsPhotoshop');
+      },
+      skillsNode() {
+        return this.$t('skillsNode');
+      },
+      skillsReact() {
+        return this.$t('skillsReact');
+      },
+      skillsBeer() {
+        return this.$t('skillsBeer');
+      },
+      skillBeerTitle() {
+        return this.$t('skillsBeerTitle');
+      }
     }
   }
 </script>
