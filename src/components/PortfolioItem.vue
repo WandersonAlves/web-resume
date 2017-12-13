2738c0d106d92702079b1c7e92cd3142dcb5b5b8
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-12-13 00:19:00 -0300

    feat(portfolio): first items inserted; wip portfolio-items component

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,49 @@
+<style lang="sass" scoped>
+.portfolio-img 
+    width: 220px
+    height: 220px
+
+.line-height-1
+    line-height: 1em
+.private-text-center
+
+@media(max-width: 959px)
+    .private-text-center
+        text-align: center
+</style>
+<template>
+<div class="flex flex-dir-row flex-align-items-center">    
+    <img class="portfolio-img" :src="imageSource"/>
+    <div class="flex flex-dir-col">
+        <h3 class="private-text-center flex-33 white montserrat font-weight-400 line-height-1">{{portfolioItem.name}}</h3>
+        <h4 class="private-text-center flex-33 white quicksand font-weight-300 word-wrap line-height-1">{{portfolioItem.title1}}</h4>
+        <h4 class="private-text-center flex-33 white quicksand font-weight-300 line-height-1">{{portfolioItem.title2}}</h4>
+    </div>
+</div>
+  
+</template>
+<script>
+export default {
+  name: "PortfolioItem",
+  props: {
+    portfolioItem: {
+      type: Object
+    }
+  },
+  computed: {
+    imageSource() {
+      return `/src/assets/portfolio/${this.portfolioItem.img}`;
+    },
+    title1Trunc() {
+      let str = this.portfolioItem.title1;
+      if (str.length > 45) {
+        return str.substring(0, 45 - "...".length) + "...";
+      } else {
+        return str;
+      }
+    }
+  }
+};
+</script>
+
+
--- src/sections/Portfolio.vue
+++ src/sections/Portfolio.vue
@@ -9,15 +9,43 @@
                     <div>
                         <h1 class="montserrat heading-margin pink bg-white heading-bg">{{'portfolioHeader' | translate | uppercase}}</h1>
                     </div>
+                    <div class="flex flex-dir-row flex-wrap">
+                        <portfolio-item class="flex-50" v-for="(item, index) in portfolioItens" :portfolioItem="item" :key='index'></portfolio-item>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </template>
 <script>
+import PortfolioItem from '../components/PortfolioItem.vue';
 export default {
     name: 'Portfolio',
     components: {
+        PortfolioItem
+    },
+    data() {
+        return {
+            portfolioItens: [{
+                img: 'santander-lojistica.png',
+                name: 'Santander Lojistica',
+                title1: 'Application that helps clients and merchants to simulate and finish floatation on Santander platform',
+                title2: 'Fev/2017 to Jul/2017'
+            },
+            {
+                img: 'beercalc.png',
+                name: 'BeerCalc App!',
+                title1: 'PoC of CI/CD process and PWA application using AngularJS and NodeJS.',
+                title2: 'Ago/2016 - Fev/2017'
+            },
+            {
+                img: 'santander-lojistica.png',
+                name: 'Santander Lojistica',
+                title1: 'Application that helps clients and merchants to simulate and finish floatation on Santander platform',
+                title2: 'Fev/2017 to Jul/2017'
+            },
+            ]
+        }
     }
 }
 </script>
