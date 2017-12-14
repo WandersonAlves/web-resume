66341d3111d6a7424d650430ee3cb756272f817b
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-12-13 23:54:00 -0300

    feat(portfolio): final layout of portfolio-item complete

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -1,23 +1,56 @@
 <style lang="sass" scoped>
-.portfolio-img 
-    width: 220px
-    height: 220px
+=square-pseudo()
+  border-radius: 50%
+  max-width: 10px
+  max-height: 10px
+  min-height: 10px
+  min-width: 10px
+  content: " "
+  background-color: white
+  position: relative
 
 .line-height-1
     line-height: 1em
-.private-text-center
+
+.square
+  border-radius: 20px
+  max-width: 128px
+  max-height: 128px
+  min-height: 128px
+  min-width: 128px
+  margin: 10px
+
+.square::after
+  +square-pseudo()
+  top: -46px
+  left: -77px
+
+.square::before
+  +square-pseudo()
+  top: 46px
+  left: 77px
+
+.link
+  cursor: pointer
+
+.text-spacing
+  margin: 4px
 
 @media(max-width: 959px)
     .private-text-center
         text-align: center
+    .square
+        flex-direction: row !important
 </style>
 <template>
 <div class="flex flex-dir-row flex-align-items-center">    
-    <img class="portfolio-img" :src="imageSource"/>
+    <div class="square flex flex-dir-row flex-align-items-center flex-content-center link" :style="{'background-color': randomColor}">
+      <h2 class="text-center white montserrat font-weight-600 margin-reset font-size-6">{{initialLetter}}</h2>
+    </div>
     <div class="flex flex-dir-col">
-        <h3 class="private-text-center flex-33 white montserrat font-weight-400 line-height-1">{{portfolioItem.name}}</h3>
-        <h4 class="private-text-center flex-33 white quicksand font-weight-300 word-wrap line-height-1">{{portfolioItem.title1}}</h4>
-        <h4 class="private-text-center flex-33 white quicksand font-weight-300 line-height-1">{{portfolioItem.title2}}</h4>
+        <h3 class="text-spacing private-text-center flex-33 white montserrat margin-reset font-weight-400 line-height-1">{{portfolioItem.name}}</h3>
+        <p class="text-spacing private-text-center flex-33 white quicksand margin-reset font-weight-300 word-wrap line-height-1">{{portfolioItem.title1}}</p>
+        <p class="text-spacing private-text-center flex-33 white quicksand margin-reset font-weight-300 line-height-1">{{portfolioItem.title2}}</p>
     </div>
 </div>
   
@@ -31,9 +64,6 @@ export default {
     }
   },
   computed: {
-    imageSource() {
-      return `/src/assets/portfolio/${this.portfolioItem.img}`;
-    },
     title1Trunc() {
       let str = this.portfolioItem.title1;
       if (str.length > 45) {
@@ -41,6 +71,17 @@ export default {
       } else {
         return str;
       }
+    },
+    initialLetter() {
+      return this.portfolioItem.name.substring(0,1);
+    },
+    randomColor() {
+      const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+      const randomByte = () => randomNumber(0, 255)
+      const randomPercent = () => (randomNumber(50, 100) * 0.01).toFixed(2)
+      const randomCssRgba = () => `rgba(${[randomByte(), randomByte(), randomByte(), randomPercent()].join(',')})`
+      let randomColor = randomCssRgba();
+      return randomColor;
     }
   }
 };
--- src/sections/Portfolio.vue
+++ src/sections/Portfolio.vue
@@ -10,7 +10,7 @@
                         <h1 class="montserrat heading-margin pink bg-white heading-bg">{{'portfolioHeader' | translate | uppercase}}</h1>
                     </div>
                     <div class="flex flex-dir-row flex-wrap">
-                        <portfolio-item class="flex-50" v-for="(item, index) in portfolioItens" :portfolioItem="item" :key='index'></portfolio-item>
+                        <portfolio-item class="flex-50 margin-10" v-for="(item, index) in portfolioItens" :portfolioItem="item" :key='index'></portfolio-item>
                     </div>
                 </div>
             </div>
@@ -27,21 +27,23 @@ export default {
     data() {
         return {
             portfolioItens: [{
-                img: 'santander-lojistica.png',
-                name: 'Santander Lojistica',
+                name: 'Santander Lojistica (Web+Mobile)',
                 title1: 'Application that helps clients and merchants to simulate and finish floatation on Santander platform',
                 title2: 'Fev/2017 to Jul/2017'
             },
             {
-                img: 'beercalc.png',
-                name: 'BeerCalc App!',
+                name: 'BeerCalc App! (Web+Mobile+PWA)',
                 title1: 'PoC of CI/CD process and PWA application using AngularJS and NodeJS.',
                 title2: 'Ago/2016 - Fev/2017'
             },
             {
-                img: 'santander-lojistica.png',
-                name: 'Santander Lojistica',
-                title1: 'Application that helps clients and merchants to simulate and finish floatation on Santander platform',
+                name: 'Philips Healthcare Tasy (Web)',
+                title1: 'Complete and integrated hospital management software pioneer in Brazil, Tasy is a solution for hospitals, clinics, managed care plans, laboratories and diagnostic imaging centers. Developed in AngularJS using requireJS as dependency control.',
+                title2: 'Fev/2017 to Jul/2017'
+            },
+            {
+                name: 'TIM Network (Web)',
+                title1: 'Development of web systems for management and maintenance of telephone lines and financial area.',
                 title2: 'Fev/2017 to Jul/2017'
             },
             ]
