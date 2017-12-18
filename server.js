5a9cafbdf8b5ce9c166c4ff0df2e90968e521711
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-12-18 00:06:04 -0300

    feat(app): trying prerender-node

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.NODE_ENV;
 
 app.use(compression());
+app.use(require('prerender-node').set('prerenderToken', 'tpyqRIMLw4teOnaAI8FM'));
 app.use('/', express.static(__dirname));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + `/index.html`));
